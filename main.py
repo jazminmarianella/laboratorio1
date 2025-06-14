306fc869b3c21193427df411ed451a44707c725a
jazminmarianella <jazminmarianella574@gmail.com>
2025-06-13 23:55:28 -0300

    Agregando archivo inicial main para ejecucion del proceso

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-from os import path
+from os import path, listdir
 from data.data_saver import DataSaver
 from domain.dataset_csv import DatasetCSV
 from domain.dataset_json import DatasetJSON
-
-
-# Ruta de los datasets
-csv_path = path.join(path.dirname(__file__), "files/.csv")
-json_path = path.join(path.dirname(__file__), "files/.json")
+from domain.dataset_xlsx import DatasetXLSX
 
 # Cargar los datasets y transformar los datos
-csv = DatasetCSV(csv_path)
-csv.cargar_datos()
+db_saver = DataSaver()
 
-json = DatasetJSON(json_path)
-json.cargar_datos()
-
-#TODO: Validacion de datos
-
-
-# Guardar en la base de datos
-db = DataSaver()
-db.guardar_dataframe(csv.datos, "")
-db.guardar_dataframe(json.datos, "")
+#Por cada archivo del directorio files, se crea un objeto segun la extension del archivo
+for file in listdir("files/"):
+    file_path = path.join("files", file)
+    if file.endswith('.csv'):
+        dataset = DatasetCSV(file_path)
+    elif file.endswith('.json'):
+        dataset = DatasetJSON(file_path)
+    elif file.endswith('.xlsx'):
+        dataset = DatasetXLSX(file_path)
+    else:
+        print(f"Archivo {file} no soportado")
+        continue
+    
+    # Cargar los datos
+    dataset.cargar_datos()
+    #Guardar los datos en la base de datos
+    nombre_tabla = file.split('.')[0].lower()
+    db_saver.guardar_dataframe(dataset.datos, nombre_tabla)
+    print('------------------------------------------')
